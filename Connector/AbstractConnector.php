b7ac87ce22e411c0f9406abe56b59bc370e850a8
Sergei Beljakov <sergei.beljakov@andevis.ee>
2015-07-14 17:18:36 +0300

    Fixed an issue with custom return and cancel URLs

--- Connector/AbstractConnector.php
+++ Connector/AbstractConnector.php
@@ -8,11 +8,12 @@ use TFox\PangalinkBundle\Exception\BadSignatureException;
 use TFox\PangalinkBundle\Exception\UnsupportedServiceIdException;
 use TFox\PangalinkBundle\Exception\CannotGenerateSignatureException;
 use TFox\PangalinkBundle\Exception\MissingMandatoryParameterException;
+
 /**
  * Common bank connection routine
  *
  */
-abstract class AbstractConnector 
+abstract class AbstractConnector
 {
 
 
@@ -28,37 +29,38 @@ abstract class AbstractConnector
     protected $configuration;
 
     /**
-     * 
+     *
      * @var \TFox\PangalinkBundle\Service\PangalinkService
      */
     protected $pangalinkService;
-    
+
     /**
      * Address of the service
      */
     protected $serviceUrl;
-    
+
     const PATH_IMAGES = 'bundles/tfoxpangalink/img';
-    
+
     // Payment
     public abstract function createPaymentRequest();
-    public abstract function createPaymentResponse(Request  $request);
-    
+
+    public abstract function createPaymentResponse(Request $request);
+
 
     public function __construct($pangalinkService, $accountId, $configuration)
     {
-	$this->accountId = $accountId;
-	$this->pangalinkService = $pangalinkService;
-	if(is_array($this->configuration)) {
-	    $this->configuration = array_merge($this->configuration, $configuration);
-	} else {
-	    $this->configuration = $configuration;
-	}
+        $this->accountId = $accountId;
+        $this->pangalinkService = $pangalinkService;
+        if (is_array($this->configuration)) {
+            $this->configuration = array_merge($this->configuration, $configuration);
+        } else {
+            $this->configuration = $configuration;
+        }
 
-	// Assign a custom service URL
-	if(true == array_key_exists('service_url', $this->configuration)) {
-	    $this->setServiceUrl($this->configuration['service_url']);
-	}
+        // Assign a custom service URL
+        if (true == array_key_exists('service_url', $this->configuration)) {
+            $this->setServiceUrl($this->configuration['service_url']);
+        }
     }
 
     /**
@@ -66,101 +68,100 @@ abstract class AbstractConnector
      */
     public function generateReturnUrl()
     {
-	$urlReturn = null;
-	if(true == array_key_exists('route_return', $this->configuration)) {
-	    $urlReturn = $this->pangalinkService
-		->getRouter()
-		->generate($this->configuration['route_return'], array(), true);
-	}
-	if(true == is_null($urlReturn)) {
-	    if(false == array_key_exists('url_return', $this->configuration)) {
-		throw new \Exception(sprintf('Neither return URL nor return route is specified for connector "%s"', 
-		    $this->accountId));
-	    }
-	    $urlReturn = $this->configuration['url_return'];
-	}
-	return $urlReturn;
+        $urlReturn = null;
+        if (true == array_key_exists('route_return', $this->configuration)) {
+            $urlReturn = $this->pangalinkService
+                ->getRouter()
+                ->generate($this->configuration['route_return'], array(), true);
+        }
+        if (true == is_null($urlReturn)) {
+            if (false == array_key_exists('url_return', $this->configuration)) {
+                throw new \Exception(sprintf('Neither return URL nor return route is specified for connector "%s"',
+                    $this->accountId));
+            }
+            $urlReturn = $this->configuration['url_return'];
+        }
+        return $urlReturn;
     }
-    
+
     /**
      * Generates a cancel url depend on url or route was specified
      */
     public function generateCancelUrl()
     {
-	$urlCancel = null;
-	if(true == array_key_exists('route_cancel', $this->configuration)) {
-	    $urlCancel = $this->pangalinkService
-		->getRouter()
-		->generate($this->configuration['route_cancel'], array(), true);
-	}
-	if(true == is_null($urlCancel)) {
-	    if(false == array_key_exists('url_cancel', $this->configuration)) {
-		throw new \Exception(sprintf('Neither cancel URL nor cancel route is specified for connector "%s"', 
-		    $this->accountId));
-	    }
-	    $urlCancel = $this->configuration['url_cancel'];
-	}
-	return $urlCancel;
+        $urlCancel = null;
+        if (true == array_key_exists('route_cancel', $this->configuration)) {
+            $urlCancel = $this->pangalinkService
+                ->getRouter()
+                ->generate($this->configuration['route_cancel'], array(), true);
+        }
+        if (true == is_null($urlCancel)) {
+            if (false == array_key_exists('url_cancel', $this->configuration)) {
+                throw new \Exception(sprintf('Neither cancel URL nor cancel route is specified for connector "%s"',
+                    $this->accountId));
+            }
+            $urlCancel = $this->configuration['url_cancel'];
+        }
+        return $urlCancel;
     }
-    
+
     /**
      * Returns an array "id" => "file name" for images
      */
     public abstract function getButtonImagesMapping();
-    
+
     /**
      * Returns a relative path to image
      */
     public function getButtonImagePath($imageId)
     {
-	$mapping = $this->getButtonImagesMapping();
-	if(false == array_key_exists($imageId, $mapping)) {
-	    throw new \Exception(sprintf('An image with key "%s" for connector "%s" not found', 
-		    $imageId, $this->accountId));
-	}
-	return sprintf('%s%s%s', self::PATH_IMAGES, DIRECTORY_SEPARATOR, $mapping[$imageId]);
+        $mapping = $this->getButtonImagesMapping();
+        if (false == array_key_exists($imageId, $mapping)) {
+            throw new \Exception(sprintf('An image with key "%s" for connector "%s" not found',
+                $imageId, $this->accountId));
+        }
+        return sprintf('%s%s%s', self::PATH_IMAGES, DIRECTORY_SEPARATOR, $mapping[$imageId]);
     }
-    
 
-    
+
     /*
      * Get/Set properties
      */
-    
+
     public function getConfiguration()
     {
-	return $this->configuration;
+        return $this->configuration;
     }
-    
+
     public function getConfigurationValue($key)
     {
-	if(true == array_key_exists($key, $this->configuration))
-	    return $this->configuration[$key];
-	
-	return null;
+        if (true == array_key_exists($key, $this->configuration))
+            return $this->configuration[$key];
+
+        return null;
     }
-   
+
     public function setServiceUrl($serviceUrl)
     {
-	$this->serviceUrl = $serviceUrl;
-	return $this;
+        $this->serviceUrl = $serviceUrl;
+        return $this;
     }
-    
+
     public function getServiceUrl()
     {
-	return $this->serviceUrl;
+        return $this->serviceUrl;
     }
-    
+
     /**
      * @return \TFox\PangalinkBundle\Service\PangalinkService
      */
     public function getPangalinkService()
     {
-	return $this->pangalinkService;
+        return $this->pangalinkService;
     }
 
     public function getAccountId()
     {
-	return $this->accountId;
+        return $this->accountId;
     }
 }
--- Request/IPizza/Payment/SwedbankPaymentRequest.php
+++ Request/IPizza/Payment/SwedbankPaymentRequest.php
@@ -13,64 +13,62 @@ class SwedbankPaymentRequest extends AbstractIPizzaPaymentRequest
      * var \TFox\PangalinkBundle\Connector\IPizza\SwedbankConnector
      */
     protected $connector;
-    
-    public function __construct(SwedbankConnector $connector) 
+
+    public function __construct(SwedbankConnector $connector)
     {
-	$this->connector = $connector;
+        $this->connector = $connector;
     }
-    
+
     public function initFormFields()
     {
-	parent::initFormFields();
-	
-	$this
-	    ->setVendorId($this->connector->getConfigurationValue('vendor_id'))
-	    ->setCurrency('EUR')
-	    ->setRecipientAccount($this->connector->getConfigurationValue('account_number'))
-	    ->setRecipientName($this->connector->getConfigurationValue('account_owner'))
-	    ->setReferenceNumber('')
-	    ->setLanguage('EST')
-	    ->setUrlReturn($this->connector->generateReturnUrl())
-	    ->setUrlCancel($this->connector->generateCancelUrl())
-	    ->setEncoding('UTF-8')
-	    ->setServiceUrl($this->connector->getServiceUrl())
-	    ->setServiceId('1011')
-	    ->setVersion('008')
-	;
-	
+        parent::initFormFields();
+
+        $this
+            ->setVendorId($this->connector->getConfigurationValue('vendor_id'))
+            ->setCurrency('EUR')
+            ->setRecipientAccount($this->connector->getConfigurationValue('account_number'))
+            ->setRecipientName($this->connector->getConfigurationValue('account_owner'))
+            ->setReferenceNumber('')
+            ->setLanguage('EST')
+            ->setEncoding('UTF-8')
+            ->setServiceUrl($this->connector->getServiceUrl())
+            ->setServiceId('1011')
+            ->setVersion('008');
     }
-    
+
     public function getFormData()
     {
-	$formData = $this->formFields;
-	
-	$datetime = $this->getDateTime();
-	if($datetime instanceof \DateTime) {
-	    $strtime = sprintf('%sT%s', 
-		$datetime->format('Y-m-d'),
-		$datetime->format('H:i:sO')
-	    );
-	    $datetime = $strtime;
-	    $formData[$this->formFieldsMapping[AbstractPaymentRequest::FORM_FIELD_DATETIME]] = $datetime;
-	}
-	
-	$macFields = array($this->getServiceId(), $this->getVersion(), $this->getVendorId(), $this->getTransactionId(), 
-		$this->getAmount(), $this->getCurrency(), $this->getRecipientAccount(), $this->getRecipientName(),
-		$this->getReferenceNumber(), $this->getComment(), $this->getUrlReturn(), $this->getUrlCancel(),
-		$datetime);
-	$macData = array_map(function($macField) {
-	    return sprintf('%s%s', 
-		str_pad(mb_strlen($macField, "UTF-8"), 3, "0", STR_PAD_LEFT),
-		$macField
-	    );
-	}, $macFields);
-	$macData = implode('', $macData);	
+        $formData = $this->formFields;
 
-	$privateKey = $this->getPrivateKey();
-	$signature = null;
-	openssl_sign ($macData, $signature, $privateKey, OPENSSL_ALGO_SHA1);
-	$formData["VK_MAC"] = base64_encode($signature);
-	
-	return $formData;
+        $datetime = $this->getDateTime();
+        if ($datetime instanceof \DateTime) {
+            $strtime = sprintf('%sT%s',
+                $datetime->format('Y-m-d'),
+                $datetime->format('H:i:sO')
+            );
+            $datetime = $strtime;
+            $formData[$this->formFieldsMapping[AbstractPaymentRequest::FORM_FIELD_DATETIME]] = $datetime;
+        }
+
+        $urlReturn = $this->getUrlReturn() ? $this->getUrlReturn() : $this->connector->generateReturnUrl();
+        $urlCancel = $this->getUrlCancel() ? $this->getUrlCancel() : $this->connector->generateCancelUrl();
+        $macFields = array($this->getServiceId(), $this->getVersion(), $this->getVendorId(), $this->getTransactionId(),
+            $this->getAmount(), $this->getCurrency(), $this->getRecipientAccount(), $this->getRecipientName(),
+            $this->getReferenceNumber(), $this->getComment(), $urlReturn, $urlCancel,
+            $datetime);
+        $macData = array_map(function ($macField) {
+            return sprintf('%s%s',
+                str_pad(mb_strlen($macField, "UTF-8"), 3, "0", STR_PAD_LEFT),
+                $macField
+            );
+        }, $macFields);
+        $macData = implode('', $macData);
+
+        $privateKey = $this->getPrivateKey();
+        $signature = null;
+        openssl_sign($macData, $signature, $privateKey, OPENSSL_ALGO_SHA1);
+        $formData["VK_MAC"] = base64_encode($signature);
+
+        return $formData;
     }
 }
